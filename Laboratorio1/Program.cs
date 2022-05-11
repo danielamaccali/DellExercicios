0f69a2a407cffb0fa2301f7424a53be7923defd6
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-11 18:58:45 -0300

    Exercicios laboratorio 1

---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,17 @@
+﻿byte b;
+
+b = byte.MaxValue;
+
+Console.WriteLine($"Valor máximo de byte: {b}");
+
+int i;
+
+i = int.MaxValue;
+
+Console.WriteLine($"Valor máximo de inteiro: {i}");
+
+long l;
+
+l = long.MaxValue;
+
+Console.WriteLine($"Valor máximo de long: {l}");
