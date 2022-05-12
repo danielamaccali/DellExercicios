bc87187842a460c3fac318d482d0ba23c99729fb
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-12 18:29:04 -0300

    Exercicios

--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -16,3 +16,15 @@ string strTerceira = strPrimeira + strSegunda;
 int cchTamanho = strTerceira.Length;
 string strQuarta = "Tamanho = " + cchTamanho.ToString();
 Console.WriteLine(strQuarta);
+Console.WriteLine(strTerceira.Substring(0, 5));
+
+DateTime dt = new DateTime(2022, 05, 02);
+string strQuinta = dt.ToString();
+Console.WriteLine(strQuinta);
+
+//Exercícios
+
+float fl = float.MaxValue;
+double db = double.MaxValue;
+decimal dc = decimal.MaxValue;
+Console.WriteLine($"O tamanho máximo de: \n float: {fl} \n double: {db} \n decimal: {dc} \n ");
---
+++ laboratorio2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Laboratório 2 - Arrays
+
+int[] array = new int[5] {10, 20, 30, 40, 50};
+int i;
+for(i = 0; i < 5; i++)
+{
+    Console.WriteLine("Índice = " + i + " & Valor = " + array[i]);
+}
+
+//-----------------------------------------
+
+string[] str = new string[3];
+int iStr;
+str[0] = "Um";
+str[1] = "Dois";
+str[2] = "Três";
+for(iStr = 0; iStr <3; iStr++)
+{
+    Console.WriteLine("Indice = " + )
+}
