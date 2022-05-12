2fafb405ab55d8b13793a1ab3f9eef6fab6da24e
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-12 13:16:14 -0300

    Exercícios string

--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -1,17 +1,18 @@
 ﻿byte b;
-
 b = byte.MaxValue;
-
 Console.WriteLine($"Valor máximo de byte: {b}");
 
 int i;
-
 i = int.MaxValue;
-
 Console.WriteLine($"Valor máximo de inteiro: {i}");
 
 long l;
-
 l = long.MaxValue;
-
 Console.WriteLine($"Valor máximo de long: {l}");
+
+string strPrimeira = "Alo ";
+string strSegunda = "Mundo";
+string strTerceira = strPrimeira + strSegunda;
+int cchTamanho = strTerceira.Length;
+string strQuarta = "Tamanho = " + cchTamanho.ToString();
+Console.WriteLine(strQuarta);
