65103b0556bccde4a80f94c6ab0c97a0628a2ec9
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-12 19:06:22 -0300

    Exercicios atualizados

--- laboratorio2/Program.cs
+++ laboratorio2/Program.cs
@@ -7,7 +7,7 @@ for(i = 0; i < 5; i++)
     Console.WriteLine("Índice = " + i + " & Valor = " + array[i]);
 }
 
-//-----------------------------------------
+Console.WriteLine("----------------------");
 
 string[] str = new string[3];
 int iStr;
@@ -16,5 +16,26 @@ str[1] = "Dois";
 str[2] = "Três";
 for(iStr = 0; iStr <3; iStr++)
 {
-    Console.WriteLine("Indice = " + )
+    Console.WriteLine("Indice = " + iStr + " & Valor = " + str[iStr]);
+}
+
+Console.WriteLine("----------------------");
+
+DateTime[] dt = new DateTime[2];
+int iDate;
+dt[0] = new DateTime(2002, 5, 1);
+dt[1] = new DateTime(2002, 6, 1);
+for(iDate=0; iDate<2; iDate++)
+{
+    Console.WriteLine("Índice = " + iDate + " & Data = " + dt[iDate].ToShortDateString());
+}
+
+Console.WriteLine("----------------------");
+
+iDate = 0;
+
+foreach(DateTime item in dt)
+{
+    Console.WriteLine("Índice = " + iDate  + " & Data = " + item.ToShortDateString());
+    iDate++;
 }
