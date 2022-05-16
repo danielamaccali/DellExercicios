f0e8ac974b2eff7fc211ae5239217b0c58d8d3c9
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-16 13:02:52 -0300

    Laboratório 3

---
+++ Laboratorio3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+
+//1
+List<string> listaStrings = new List<string>();
+listaStrings.Add("Um");
+listaStrings.Add("Hello");
+listaStrings.Add("World");
+Console.WriteLine(listaStrings[0]);
+Console.WriteLine(listaStrings[1]);
+Console.WriteLine(listaStrings[2]);
+// listaStrings.Add(10); //teste inserindo tipo diferente declarado lista
+
+//2
+Console.WriteLine("SEgunda parte");
+Queue<Object> q = new Queue<object>();
+q.Enqueue(".Net Framework");
+q.Enqueue(new Decimal(123.456));
+q.Enqueue(654.321);
+Console.WriteLine(q.Dequeue());
+Console.WriteLine(q.Dequeue());
+Console.WriteLine(q.Dequeue());
+Queue<int> minhaFila = new Queue<int>();
+minhaFila.Enqueue(10);
+minhaFila.Enqueue(200);
+minhaFila.Enqueue(1000);
+Console.WriteLine(minhaFila.Dequeue());
+Console.WriteLine(minhaFila.Dequeue());
+Console.WriteLine(minhaFila.Dequeue());
+
+//3
+Console.WriteLine("Terceira parte");
+Dictionary<int, string> paises = new Dictionary<int, string>();
+paises[44] = "Reino Unido";
+paises[33] = "França";
+paises[55] = "Brasil";
+Console.WriteLine("O código 55 é: {0}", paises[55]);
