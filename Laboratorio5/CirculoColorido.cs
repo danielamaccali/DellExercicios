f06f5587a34fd3c7a4178d240c3d0316d190b7c5
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-16 19:10:49 -0300

    Atualizações

---
+++ Laboratorio5/CirculoColorido.cs
@@ -0,0 +1,26 @@
+public class CirculoColorido: Circulo
+{
+    private string minhaCor;
+
+    public CirculoColorido()
+    {
+        minhaCor = "preto";
+    }
+
+    public CirculoColorido(double x, double y, double r, string c)
+                :base(x, y, r)
+    {
+        minhaCor = c;
+    }
+
+    public string Cor
+    {
+        get{ return minhaCor;}
+        set{ minhaCor = value;}
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + "cor = " + Cor;
+    }
+}
---
+++ Laboratorio5/Program.cs
@@ -0,0 +1,11 @@
+﻿Circulo circ1 = new Circulo();
+Console.WriteLine(circ1);
+
+Circulo circ2 = new Circulo(2.4, 5, 3);
+Console.WriteLine(circ2);
+
+CirculoColorido circ3 = new CirculoColorido();
+Console.WriteLine(circ3);
+
+CirculoColorido circ4 = new CirculoColorido(1.5, 3.1, 1, "vermelho");
+Console.WriteLine(circ4);
