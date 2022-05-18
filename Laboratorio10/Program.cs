1865129f58a6e4d4bd445b0132bb5a0ab8b649b8
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-18 18:54:37 -0300

    Updating with error Lab 10

--- Laboratorio10/Program.cs
+++ Laboratorio10/Program.cs
@@ -25,8 +25,25 @@ Console.WriteLine(pessoa);
 
 // Exercício 1. Construa uma consulta que retorne as pessoas agrupadas em casadas e solteiras e também o número de pessoas casadas e solteiras
 
+Console.WriteLine("Exercício 1");
+
+var linq3 = 
+    from p in pessoas
+    where p.Casada = true
+    select p;
+
+foreach (var pessoa in linq3)
+{
+    Console.WriteLine(pessoa);
+}
 // Exercício 2. Construa uma consulta que retorne a pessoa mais velha.
 
+Console.WriteLine("Pessoa mais velha: ");
+var linq4 = pessoas.Where(p => p.DataNascimento); //implementar filtro
+foreach(var pessoa in linq4)
+{
+    Console.WriteLine(pessoa);
+}
 
 // Exercício 3. Construa uma consulta que retorne a pessoa solteira mais nova.
 
