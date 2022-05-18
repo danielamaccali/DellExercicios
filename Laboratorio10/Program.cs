33ad27e70a095f63757fe712be0f1408391bd16d
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-18 18:10:39 -0300

    Updating

---
+++ Laboratorio10/Program.cs
@@ -0,0 +1,32 @@
+﻿List<Pessoa> pessoas = new List<Pessoa>
+{
+    new Pessoa{Nome="Ana",DataNascimento=new DateTime(1980,3,14), Casada=true},
+    new Pessoa{Nome="Paulo",DataNascimento=new DateTime(1978,10,23), Casada=true},
+    new Pessoa{Nome="Maria",DataNascimento=new DateTime(2000,1,10), Casada=false},
+    new Pessoa{Nome="Carlos",DataNascimento=new DateTime(1999,12,12), Casada=false}
+};
+
+var linq1 =
+    from p in pessoas
+    where p.Casada && p.DataNascimento >= new DateTime(1980, 1, 1)
+    select p;
+
+foreach (var pessoa in linq1)
+{
+    Console.WriteLine(pessoa);
+}
+
+var linq2 = pessoas.Where(p => p.Casada && p.DataNascimento >= new DateTime(1980, 1, 1));
+foreach (var pessoa in linq2)
+{
+Console.WriteLine("\n Linq escrito de outra forma - mesmo resultado:");
+Console.WriteLine(pessoa);
+}
+
+// Exercício 1. Construa uma consulta que retorne as pessoas agrupadas em casadas e solteiras e também o número de pessoas casadas e solteiras
+
+// Exercício 2. Construa uma consulta que retorne a pessoa mais velha.
+
+
+// Exercício 3. Construa uma consulta que retorne a pessoa solteira mais nova.
+
