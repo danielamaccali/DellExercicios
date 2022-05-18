ae8446c434c80d3f889fc219bd99531b7632ad31
Daniela Maccali <daniela.maccali@gmail.com>
2022-05-18 17:53:06 -0300

    Updating

---
+++ Laboratorio9/Program.cs
@@ -0,0 +1,16 @@
+﻿// inicializa o termometro e mostra seu valor
+TermometroLimite term = new TermometroLimite(5);
+Console.WriteLine(term.ToString());
+
+// adiciona um tratador ao evento LimiteSuperiorEvent
+term.LimiteSuperiorEvent += new TermometroLimite.MeuDelegate(TrataLimiteSuperior);
+
+// aumentar a temperatura além do limite
+term.Aumentar(6);
+Console.WriteLine(term.ToString());
+
+static void TrataLimiteSuperior(string msg) //tirei o private que tinha no exercício
+{
+Console.WriteLine(msg);
+}
+
